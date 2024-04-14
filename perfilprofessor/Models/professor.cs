e9ebb50ecd06e658f1f2c32fc980305db5e20311
arthurviotti <arthur.viotti@hotmail.com>
2024-04-14 18:40:05 -0300

    Configuração do banco de dados e da tabela professor

---
+++ perfilprofessor/Models/professor.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace perfilprofessor.Models
+{
+    [Table("Professores")]
+    public class Professor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Nome de usuário!")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o E-mail!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a senha!")]
+        public string Senha { get; set; }
+
+    }
+}
